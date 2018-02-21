0295cc9023d380959763f883a142c6279571106f
Kris Winer <kriswiner@users.noreply.github.com>
2018-02-20 18:56:21 -0800

    Update BME280_VEML6040_Ladybug_new.ino

--- BME280_VEML6040/BME280_VEML6040_Library/BME280_VEML6040_Ladybug_new/BME280_VEML6040_Ladybug_new.ino
+++ BME280_VEML6040/BME280_VEML6040_Library/BME280_VEML6040_Ladybug_new/BME280_VEML6040_Ladybug_new.ino
@@ -45,8 +45,8 @@ Adafruit_SharpMem display(DSCK, DMOSI, DSS);
 // Specify BME280 configuration
 uint8_t Posr = P_OSR_16, Hosr = H_OSR_16, Tosr = T_OSR_02, Mode = normal, IIRFilter = BW0_042ODR, SBy = t_1000ms;     // set pressure amd temperature output data rate
 
-uint32_t rawPress, rawTemp, compHumidity, compTemp, compPress;   // pressure, humidity, and temperature raw count output for BME280
-uint16_t rawHumidity;  // variables to hold raw BME280 humidity value
+int32_t rawPress, rawTemp, compHumidity, compTemp, compPress;   // pressure, humidity, and temperature raw count output for BME280
+int16_t rawHumidity;  // variables to hold raw BME280 humidity value
 
 float temperature_C, temperature_F, pressure, humidity, altitude; // Scaled output of the BME280
 
